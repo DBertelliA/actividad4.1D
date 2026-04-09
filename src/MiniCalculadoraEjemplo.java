4ba0021e70e7e97ba5031ebf18b4d6fc8ccc5f35
dbertelli <useromegad@gmail.com>
2026-04-09 13:35:59 +0200

    Documentacion de Raiz Cuadrada

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -1,4 +1,22 @@
+/**
+ * Clase que engloba una serie de metodos que tienen como objetivos hacer
+ * la raiz cuadrada y el valor absoluto respectivamente
+ *
+ * @author Daniel
+ * @version 1.0
+ * @since 09-04-2026
+ */
 public class MiniCalculadoraEjemplo {
+    /**
+     * Metodo que devuelve la raiz cuadrada de un numero de valor double que se le pase
+     * usando la biblioteca Math
+     *
+     * @author Daniel
+     * @version 1.0
+     * @param numero
+     * @return Raiz cuadrada del parametro pasado de tipo double
+     * @since 09-04-2026
+     */
     public double raizCuadrada(double numero){
         return Math.sqrt(numero);
     }
