fc454f5affb17888191d49b6eb1b0b67d77a970e
dbertelli <useromegad@gmail.com>
2026-04-09 13:33:49 +0200

    Valor absoluto

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -2,4 +2,7 @@ public class MiniCalculadoraEjemplo {
     public double raizCuadrada(double numero){
         return Math.sqrt(numero);
     }
+    public double absoluto(double numero){
+        return Math.abs(numero);
+    }
 }
